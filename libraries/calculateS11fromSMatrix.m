bfc21ab6087afdcbd1ae6380a2f9abb6558ba47d
Stefan <Stefan.Umrath@dlr.de>
2018-08-10 12:31:02 +0200

    update

--- libraries/calculateS11fromSMatrix.m
+++ libraries/calculateS11fromSMatrix.m
@@ -1,15 +1,17 @@
-function [S11] = calcS11fromSMatrix(resultfile, Dlz, N);
+function [S11] = calculateS11fromSMatrix(resultfile, Dlz, Depsilon, Dkappa);
 % calculate the scattering parameter of a FSS coating on a
 % dielctric coating of thickness Dlz based on the 
 % results in the filename 'resultfile' which are for a FSS coating on top of
 % a dielectric slab of infinite thickness with index N;   
+physical_constants;
 if (typeinfo(Dlz) == 'matrix');
     error('So far only scalar values of the substrate thickness are implemented');
 end;
-d = dlmread(resultfile,',',21,1);
+d = dlmread(resultfile,',',21,0);
 f = d(:,1);
+N = sqrt(Depsilon+1j*Dkappa./(2*pi*f*EPS0));
 R = d(:,2)+1j*d(:,3);
 T = d(:,4)+1j*d(:,5);
-phase = exp(-2j*2*pi*f*N*Dlz)
-S11 = (R - phase) ./ (1 - R*phase);
+phase = exp(2j*2*pi*f.*N*Dlz/C0);
+S11 = (R - phase) ./ (1 - R.*phase);
 end
