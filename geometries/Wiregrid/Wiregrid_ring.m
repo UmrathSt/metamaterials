1239a20af52dc8c509fe8924da4e494b9e9f4575
umra_st <stefan.umrath@dlr.de>
2018-09-20 15:17:10 +0200

    update

---
+++ geometries/Wiregrid/Wiregrid_ring.m
@@ -0,0 +1,147 @@
+function Wiregrid_ring(type_of_sim, UCDim, lz, L, eps, kappa,ZMESHRES=40,MESHRES=140);
+% Setup a wiregrid slab simulation
+% intended for the calculation of transmission and reflection coefficients
+% for varying Cu edge-length in order to minimize the reflection of a stacked structure
+addpath('../../libraries');
+physical_constants;
+node = uname.nodename();
+% setup the system paths
+Paths.SimPath = 'WireGrid_ring';
+Paths.SimCSX = 'WireGrid_geometry.xml';
+Paths = configureSystemPaths(Paths, node);
+addpath([Paths.ResultBasePath 'libraries/']);
+Paths.ResultPath = ['Results/SParameters/' Paths.SimPath];
+sim_setup.Paths = Paths; 
+%-----------------system path setup END---------------------------------------|
+sim_setup.Paths = Paths;
+sim_setup.FDTD.Write = 'True';
+sim_setup.FDTD.numThreads = 5;
+sim_setup.FDTD.Run = 'True';
+sim_setup.FDTD.fstart = 3e9;
+sim_setup.FDTD.fstop = 40e9;
+sim_setup.FDTD.EndCriteria = 1e-6;
+sim_setup.FDTD.Kinc = [0,0,-1];
+sim_setup.FDTD.Polarization = [1,0,0];
+sim_setup.FDTD.PML = 'MUR';
+sim_setup.Geometry.Show = 'True';
+sim_setup.Geometry.grounded = 'False';
+if strcmp(type_of_sim,'LEFT') || strcmp(type_of_sim,'RIGHT') || strcmp(type_of_sim,'LEFTRIGHT');
+    sim_setup.Geometry.grounded = 'False';
+end;
+sim_setup.Geometry.MeshResolution = [MESHRES,MESHRES,ZMESHRES];
+sim_setup.Geometry.Unit = 1e-3;
+
+sim_setup.Geometry.UCDim = [UCDim, UCDim]; % size of the unit-cell in the xy-plane
+SParameters.df = 10e6;
+SParameters.fstart = sim_setup.FDTD.fstart;
+SParameters.fstop = sim_setup.FDTD.fstop;
+
+SParameters.ResultFilename = ['UCDim_' num2str(UCDim) '_L_' num2str(L) '_eps_' num2str(eps) '_kappa_' num2str(kappa) '_' type_of_sim '_lz_' num2str(lz)];
+
+TDDump.Status = 'False';
+FDDump.Status = 'False';
+PP.DoSPararmeterDump = 'True';
+PP.TDDump = TDDump;
+PP.FDDump = FDDump;
+PP.SParameters = SParameters;
+sim_setup.PP = PP;
+% Define the materials which are used:
+mFR4.Name = 'FR4';
+mFR4.Type = 'Material';
+mFR4.Properties.Kappa = kappa;
+mFR4.Properties.Epsilon = eps;
+mCuSheet.Name = 'CuSheet';
+mCuSheet.Type = 'ConductingSheet';
+mCuSheet.Properties.Thickness = 0;
+mCuSheet.Properties.Kappa = 56e6;
+materials{1} = mFR4;
+materials{2} = mCuSheet;
+if strcmp(type_of_sim, 'LEFT');
+    lMaterial = mFR4;
+    lEpsilon = lMaterial.Properties.Epsilon;
+    lKappa = lMaterial.Properties.Kappa;
+    sim_setup.PP.lEpsilon = lMaterial.Properties.Epsilon;
+    sim_setup.PP.lKappa   = lMaterial.Properties.Kappa;
+    sim_setup.Geometry.lMaterial = lMaterial;
+elseif strcmp(type_of_sim,'RIGHT');
+    rMaterial = mFR4;
+    rEpsilon = rMaterial.Properties.Epsilon;
+    rKappa = rMaterial.Properties.Kappa;
+    sim_setup.PP.rEpsilon = rMaterial.Properties.Epsilon;
+    sim_setup.PP.rKappa   = rMaterial.Properties.Kappa;
+    sim_setup.Geometry.rMaterial = rMaterial;
+elseif strcmp(type_of_sim,'LEFTRIGHT');
+    rMaterial = mFR4;
+    rEpsilon = rMaterial.Properties.Epsilon;
+    rKappa = rMaterial.Properties.Kappa;
+    sim_setup.PP.rEpsilon = rMaterial.Properties.Epsilon;
+    sim_setup.PP.rKappa   = rMaterial.Properties.Kappa;
+    sim_setup.Geometry.rMaterial = rMaterial;
+    lMaterial = mFR4;
+    lEpsilon = lMaterial.Properties.Epsilon;
+    lKappa = lMaterial.Properties.Kappa;
+    sim_setup.PP.lEpsilon = lMaterial.Properties.Epsilon;
+    sim_setup.PP.lKappa   = lMaterial.Properties.Kappa;
+    sim_setup.Geometry.lMaterial = lMaterial;
+end;
+
+fc = (sim_setup.FDTD.fstart+sim_setup.FDTD.fstop)/2;
+
+
+% End of material definition
+% Define the objects which are made of the defined materials
+oFR4Slab.Name = 'FR4Background';
+oFR4Slab.MName = 'FR4';
+oFR4Slab.Type = 'Box';
+oFR4Slab.Thickness = lz;
+oFR4Slab.Prio = 2;
+oFR4Slab.Bstart = [-UCDim/2, -UCDim/2, 0];
+oFR4Slab.Bstop = [+UCDim/2, +UCDim/2, oFR4Slab.Thickness];
+
+oRect1.Name = 'CopperRect';
+oRect1.MName = 'CuSheet';
+oRect1.Type = 'Polygon';
+oRect1.Thickness = 0;
+oRect1.Prio = 4;
+oRect1.Points = [[-L/2;UCDim/2],[L/2;UCDim/2],...
+                           [L/2;L/2],[UCDim/2;L/2],...
+                           [UCDim/2;-L/2],[L/2;-L/2],...
+                           [L/2;-UCDim/2],[-L/2;-UCDim/2],...
+                           [-L/2;-L/2],[-UCDim/2;-L/2],...
+                           [-UCDim/2;L/2],[-L/2;L/2],...
+                           [-L/2;UCDim/2]];
+oSQ1.Name = 'CopperRect_outer';
+oSQ1.MName = 'CuSheet';
+oSQ1.Type = 'Polygon';
+oSQ1.Thickness = 0;
+oSQ1.Prio = 4;
+oSQ1.Points = [[L/2;L/2],[UCDim/2;L/2],[UCDim/2;UCDim/2],[L/2;UCDim/2]];
+oSQ2 = oSQ1;
+oSQ3 = oSQ1;
+oSQ4 = oSQ1;
+
+oSQ2.Points = [[-L/2;L/2],[-UCDim/2;L/2],[-UCDim/2;UCDim/2],[-L/2;UCDim/2]];
+oSQ3.Points = [[-L/2;-L/2],[-UCDim/2;-L/2],[-UCDim/2;-UCDim/2],[-L/2;-UCDim/2]];
+oSQ4.Points = [[L/2;-L/2],[UCDim/2;-L/2],[UCDim/2;-UCDim/2],[L/2;-UCDim/2]];
+
+layer1.Name = 'FSS';
+layer1.Thickness = 0;
+layer1.objects{1} = oRect1;
+layer2.Name = 'FR4';
+layer2.Thickness = lz;
+layer2.objects{1} = oFR4Slab;
+layer3.Name = 'FSS2';
+layer3.Thickness = 0;
+layer3.objects{1} = oSQ1;
+layer3.objects{2} = oSQ2;
+layer3.objects{3} = oSQ3;
+layer3.objects{4} = oSQ4;
+
+sim_setup.used_layers = {layer1,layer2,layer3,layer2,layer1};
+if strcmp(type_of_sim, 'EXACT');
+    sim_setup.used_layers = {layer1,layer2,layer3,layer2,layer1};
+end;
+sim_setup.used_materials = materials;
+
+retval = setup_simulation(sim_setup);
+end;
--- geometries/Wiregrid/sweep_WireGrid.m
+++ geometries/Wiregrid/sweep_WireGrid.m
@@ -1,9 +1,9 @@
 clc;
 clear;
 
-kappa = 0.5;
-eps = 2;
-lz = 2;
+kappa = 0;
+eps = 1;
+lz = 0.5;
 UCDim = 4
 L = linspace(3, 3.95, 20);
 type_of_sim = {"EXACT"};
@@ -11,22 +11,8 @@ ZMESHRES = 30;
 MESHRES = 300;
 for edgeL = [0.5];
     for i = 1:length(type_of_sim);
-        Wiregrid(type_of_sim{i}, UCDim, lz, edgeL, eps, kappa,ZMESHRES);
+        Wiregrid_ring(type_of_sim{i}, UCDim, lz, edgeL, eps, kappa,ZMESHRES);
     end;
 end;
 
-type_of_sim = {"EXACT"};
-lz = 1;
-edgeL = 3;
-kappa = 0.5;
-eps = 2;
-for lz = [3];
-ZMESHRES = 40;
-MESHRES = 200;
-for edgeL = [3.8];
-    for i = 1:length(type_of_sim);
-        RectCuAbsorber(type_of_sim{i}, UCDim, lz, edgeL, eps, kappa,ZMESHRES);
-    end;
-end;
-end;
 
