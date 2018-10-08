562eb140cd9a7f455868cc5f26b7e64ce07f9565
Stefan <Stefan.Umrath@dlr.de>
2018-10-08 10:03:00 +0200

    capacitively coupled RLC circuits for the description of a double ring absorber

--- python_scripts/double_RLC_fit.py
+++ python_scripts/double_RLC_fit.py
@@ -5,17 +5,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--filename", dest="filename",type=str)
 parser.add_argument("--eps", dest="eps",type=float)
 parser.add_argument("--D", dest="D",type=float)
-parser.add_argument("--R", dest="R",type=float)
-parser.add_argument("--L", dest="L",type=float)
-parser.add_argument("--C", dest="C",type=float)
+parser.add_argument("--R1", dest="R1",type=float)
+parser.add_argument("--L1", dest="L1",type=float)
+parser.add_argument("--C1", dest="C1",type=float)
+parser.add_argument("--R2", dest="R2",type=float)
+parser.add_argument("--L2", dest="L2",type=float)
+parser.add_argument("--C2", dest="C2",type=float)
+parser.add_argument("--CC", dest="CC",type=float)
 parser.add_argument("--tand", dest="tand",type=float)
 parser.add_argument("--MaxIDX", dest="MaxIDX", type=int, default=-1)
 parser.add_argument("--MinIDX", dest="MinIDX", type=int, default=0)
 
 args = parser.parse_args()
 
-def fit_func(R, L, C, f, D, eps, tand):
-    """ A Simple RLC series circuit in
+def fit_func(R1, L1, C1, R2, L2, C2, CC, f, D, eps, tand):
+    """ A two parallel RLC circuits coupled capacitively 
         parallel to the analytically known
         impedance of a grounded dielectric
         slab of thickness D
@@ -23,19 +27,20 @@ def fit_func(R, L, C, f, D, eps, tand):
     w = 2*np.pi*f
     epsilon = eps*(1-tand*1j)
     Zd = 376j/np.sqrt(epsilon)*np.tan(w/3e8*np.sqrt(epsilon)*D)
-    Zfss = R +1j*w*L + 1/(1j*w*C)
-    Ztml = Zd
-    Zges = np.conjugate((1/Zfss+1/Ztml)**(-1))
+    Z1ts = (1/(R1+1j*w*L1) + 1/(R2+1j*w*L2-1j/(w*CC)))**(-1)-1j/(w*C1)
+    Z2ts = (1/(R2+1j*w*L2) + 1/(R1+1j*w*L1-1j/(w*CC)))**(-1)-1j/(w*C2)
+    Zges = np.conjugate((1/Zd+1/Z1ts+1/Z2ts)**(-1))
     Rges = (Zges-376)/(Zges+376)
     return Zges
+
 def residuals(coeffs, S11num, f, D, eps, tand):
-    R, L, C = coeffs
-    Zges = fit_func(R, L, C, f, D, eps, tand)
+    R1, L1, C1, R2, L2, C2, CC = coeffs
+    Zges = fit_func(R1, L1, C1, R2, L2, C2, CC, f, D, eps, tand)
     Rges = (Zges-376)/(376+Zges)
     val = np.abs(Rges.real-S11num.real)+np.abs(Rges.imag-S11num.imag)
     return val
 
-x0 = [args.R, args.L, args.C]
+x0 = [args.R1, args.L1, args.C1, args.R2, args.L2, args.C2, args.CC]
 data = np.loadtxt(args.filename, delimiter=",")
 I, J = args.MinIDX, args.MaxIDX
 f, S11 = data[I:J,0], data[I:J,1]+1j*data[I:J,2]
