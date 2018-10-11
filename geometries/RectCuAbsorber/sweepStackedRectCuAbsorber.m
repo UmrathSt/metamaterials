5720406401ca421c956571e1f3936de5b7415b58
umra_st <stefan.umrath@dlr.de>
2018-10-11 14:51:52 +0200

    update

--- geometries/RectCuAbsorber/sweepStackedRectCuAbsorber.m
+++ geometries/RectCuAbsorber/sweepStackedRectCuAbsorber.m
@@ -1,12 +1,13 @@
 
 UCDim = 4;
 LZ = linspace(1,3,11);
-L = [3.65,3.5, 3.35,3.2,3.05,2.9,2.75];
-
+LL1 = [3.85,3.7, 3.55,3.4,3.25,3.1,2.95];
+LL2 = LL1-1;
 WG = 0.6
 eps = 4.6;
-kappa = 0.5;
-
+kappa = 05;
+for L = [LL1,LL2];
 for lz = [0.2,0.4,0.6];#[0.6, 0.8];
     StackedSetupRectCuAbsorber(UCDim, L, lz, kappa, eps, ZMESHRES=60,MESHRES=250);
+end;
 end;
